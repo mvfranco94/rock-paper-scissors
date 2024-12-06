cae9c1791315037f68b9a5d0791a194142db2801
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2024-12-06 15:36:49 -0300

    Create computer choice logic

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello, world!");
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  const randomOption = Math.floor(Math.random() * 3);
+
+  return options[randomOption];
+}
