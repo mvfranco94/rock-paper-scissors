3ff2270f8829c87d8491cdbe85e97d2cff4a5837
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2024-12-06 22:20:58 -0300

    Create humam choice logic

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const options = ["rock", "paper", "scissors"];
 
+let playerChoice = null;
+
 function getComputerChoice() {
   const randomOption = Math.floor(Math.random() * 3);
 
   return options[randomOption];
 }
+
+function getHumanChoice() {
+  playerChoice = +prompt("Get an option\n 0 - Rock\n 1 - Paper\n 2 - Scissors");
+}
+
+getHumanChoice();
+
+console.log(playerChoice);
