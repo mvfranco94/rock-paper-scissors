2db016b6844ad91c8bd5d7c67603d518adc239c6
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-01-14 11:27:59 -0300

    Refactor five rounds logic

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const results = document.querySelector('#results');
 
+let round = 0;
 let humanScore = 0;
 let computerScore = 0;
 
@@ -17,24 +18,26 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+  const roundBoard = document.createElement('p');
   const choices = document.createElement('p');
   const scoreboard = document.createElement('p');
 
+  roundBoard.textContent = `Round: ${round + 1}`;
+
   choices.innerText = `Human: ${humanChoice}, Computer: ${computerChoice}`;
   if (humanChoice === computerChoice) {
-    scoreboard.innerText = "\nThe round was a tie!";
+    scoreboard.innerText = "The round was a tie!";
   } else if (humanChoice === "paper" && computerChoice === "rock"
     || humanChoice === "rock" && computerChoice === "scissors"
     || humanChoice === "scissors" && computerChoice === "paper") {
       humanScore++;
-      scoreboard.innerText = "\nYou win the round!";
+      scoreboard.innerText = "You win the round!";
   } else {
     computerScore++;
-    scoreboard.innerText = "\nYou lose the round!";
+    scoreboard.innerText = "You lose the round!";
   }
 
-
-  results.innerHTML = "";
+  results.appendChild(roundBoard);
   results.appendChild(choices);
   results.appendChild(scoreboard);
 
@@ -42,20 +45,29 @@ function playRound(humanChoice, computerChoice) {
 }
 
 function playGame(humanChoice) {
-  let computerChoice = getComputerChoice();
-  playRound(humanChoice, computerChoice);
-
-  // whoWin();
+  
+  if(round < 5) {
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    round++;
+    if (round >= 5) {
+      whoWin();
+      buttons.forEach(button => button.disabled = true);
+    }
+  }
 }
 
 function whoWin(){
+  const para = document.createElement('p');
   if(humanScore > computerScore) {
-    console.log("You win the game!!!");
+    para.textContent = "You win the game!!!";
   } else if(computerScore > humanScore){
-    console.log("Computer win the game!!!");
+    para.textContent = "Computer win the game!!!";
   } else {a
-    console.log("The game was a tie!!!");
+    para.textContent = "The game was a tie!!!";
   }
+
+  results.appendChild(para);
 }
 
 const buttons = document.querySelectorAll('button')
