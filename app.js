525c3c4158172414ec6e04707970072c9643b24a
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-09-05 12:03:58 -0300

    Define computer logic to choice an option
    Implemantion of computer choice between rock,
    paper and scissors. The implemantion follow The
    pseudocode write down in pseudocode folder.
    
    The implemantion has a loop to test an scenario of
    10 computer choices

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+  // set randomNumber to a number between
+  // 0 and 2
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  if (randomNumber === 0) {
+    return "rock"
+  }
+  else if (randomNumber === 1) {
+    return "paper"
+  }
+  else {
+    return "scissors"
+  }
+
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(getComputerChoice());
+}
