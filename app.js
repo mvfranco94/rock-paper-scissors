b04e20d98f0ac2357560e09c7926c83faf0003f6
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-09-08 12:03:22 -0300

    Add playRound function
    The function control the logic of one round.

--- app.js
+++ app.js
@@ -1,6 +1,56 @@
 let humanScore = 0;
 let computerScore = 0;
 
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
+
+console.log("humanChoice: ", humanChoice);
+console.log("humanScore: ", humanScore);
+console.log("computerChoice: ", computerChoice);
+console.log("computerScore: ", computerScore);
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log("It's a draw.");
+    return;
+  }
+  else if (humanChoice === "rock") {
+    if (computerChoice === "paper") {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+      computerScore++;
+    }
+    else {
+      console.log(`You Win! ${humanChoice} beats ${computerChoice}.`)
+      humanScore++;
+    }
+  }
+  else if (humanChoice === "paper") {
+    if (computerChoice === "scissors") {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      computerScore++;
+    }
+    else {
+      console.log(`You Win! ${humanChoice} beats ${computerChoice}.`);
+      humanScore++;
+    }
+  }
+  else if (humanChoice === "scissors") {
+    if (computerChoice === "paper") {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+      computerScore++;
+    }
+    else {
+      console.log(`You Win! ${humanChoice} beats ${computerChoice}.`)
+      humanScore++;
+    }
+  }
+}
+
 function getComputerChoice() {
   // set randomNumber to a number between
   // 0 and 2
@@ -23,5 +73,3 @@ function getHumanChoice() {
   return choice;
 }
 
-console.log(getHumanChoice());
-console.log(getComputerChoice());
