31e5ac6391fce5ae1c8b4e56df95d04ef6f35902
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-09-24 11:51:14 -0300

    Add an UI to play the game
    Before the game was played in the console, and
    now the game has a basic user interface where the
    user can choose among 3 buttons.
    
    Also was included a score panel and a button to
    start and restart the game.

--- app.js
+++ app.js
@@ -1,48 +1,45 @@
+const options = document.querySelector("#options");
+const result = document.querySelector("#result");
+const humanScoreLabel = document.querySelector("#humanScore");
+const computerScoreLabel = document.querySelector("#computerScore");
+const winner = document.querySelector("#winner");
+const start = document.querySelector("#start");
+
 let humanScore = 0;
 let computerScore = 0;
+let round = 0; 
 
-playGame();
-
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-
-    playRound(humanChoice, computerChoice);
-    console.log(`human ${humanScore} x ${computerScore} computer`);
-  }
-
-  if (humanScore > computerScore) {
-    console.log("The winner was YOU! Congratulations!");
-  }
-  else if (humanScore < computerScore) {
-    console.log("The winner was the COMPUTER! Sorry, try again!");
-  }
-  else {
-    console.log("There's no winner! It was a DRAW!");
-  }
-}
+initializeGame();
+resetOptions();
 
 function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
-
-  console.log("human\tx\tcomputer");
-  console.log(`${humanChoice}\t \t${computerChoice}`);
+  round++;
 
   if (humanChoice === computerChoice) {
-    console.log("It's a draw.");
+    addRoundResult(humanChoice, computerChoice, "It's a draw.");
     return;
   }
   else if (humanChoice === "rock" && computerChoice === "paper" ||
     humanChoice === "paper" && computerChoice === "scissors" ||
     humanChoice === "scissors" && computerChoice === "rock") {
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+    
+    addRoundResult(
+      humanChoice, 
+      computerChoice, 
+      `You lose! ${computerChoice} beats ${humanChoice}.`,
+    );
     computerScore++;
-  } 
+  }
   else {
-    console.log(`You Win! ${humanChoice} beats ${computerChoice}.`)
+    addRoundResult(
+      humanChoice, 
+      computerChoice, 
+      `You Win! ${humanChoice} beats ${computerChoice}.`,
+    );
     humanScore++;
   }
+
+  
 }
 
 function getComputerChoice() {
@@ -62,8 +59,82 @@ function getComputerChoice() {
 
 }
 
-function getHumanChoice() {
-  const choice = prompt("Choose between rock, paper or scissors:");
-  return choice;
+function resetOptions() {
+  start.style.display = "inherit";
+  options.style.display = "none";
+  
 }
 
+function initializeGame() {
+  humanScore = 0;
+  computerScore = 0;
+  round = 0;
+  result.textContent = "";
+  updateScore();
+  winner.textContent = "";
+}
+
+start.addEventListener("click", () => {
+  options.addEventListener("click", optionSelected);
+  start.style.display = "none";
+  options.style.display = "inherit";
+  initializeGame();
+});
+
+
+function optionSelected(e) {
+  let humanChoice;
+  switch (e.target.id) {
+    case "rock":
+    case "paper":
+    case "scissors":
+      humanChoice = e.target.id;
+      break;
+    default:
+      return;
+  }
+  e.stopPropagation();
+  const computerChoice = getComputerChoice();
+  humanChoice = humanChoice.toLowerCase();
+  playRound(humanChoice, computerChoice);
+  updateScore();
+
+  if (round == 5) {
+    options.removeEventListener("click", optionSelected);
+    showWinner();
+    resetOptions();
+    start.textContent = "Restart?";
+  }
+
+  
+}
+
+function addRoundResult(humanChoice, computerChoice, message) {
+  const roundMessage = document.createElement("h2");
+  roundMessage.textContent = `Round: ${round}`;
+
+  const choices = document.createElement("p");
+  choices.textContent = `human -> ${humanChoice} x ${computerChoice} <- computer`;
+
+  const roundWinner = document.createElement("p");
+  roundWinner.textContent = message;
+  
+  result.appendChild(roundMessage);
+  result.appendChild(choices);
+  result.appendChild(roundWinner);
+} 
+
+function updateScore() {
+  humanScoreLabel.textContent = humanScore;
+  computerScoreLabel.textContent = computerScore;
+}
+
+function showWinner() {
+  if (humanScore === computerScore) {
+    winner.textContent = "The game was a draw";
+  } else if (humanScore > computerScore) {
+    winner.textContent = "You win the game!";
+  } else {
+    winner.textContent = "You lose the game!";
+  }
+}
